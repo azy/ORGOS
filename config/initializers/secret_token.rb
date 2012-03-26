c891a5923f81c1f6e5fa67ffb6eb5ef15679568c
azy <boite.remi@gmail.com>
2012-03-26 15:59:31 +0200

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orgos::Application.config.secret_token = '42202d22e35ff8b235f3e1a022905271331ad06eb549cc98e6f867b525e8df29d0f3f461bb4d37c26a7baa3a8cd4feb36fd1c23fd18645e1f1946794bdfa42bf'
